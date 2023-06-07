965bf0a132d21d3a9290f0b18f1dc5b0ce21bc23
Hai Lam <hailamnguyen54@gmail.com>
2023-06-07 18:17:55 +0700

    update file sign_in

--- resources/views/sign_in.blade.php
+++ resources/views/sign_in.blade.php
@@ -69,7 +69,7 @@
 				<div class="social-container">
 					<a href="#" class="social" style="color: var(--theme-title);"><i class="fab fa-facebook-f"></i></a>
 					<a href="#" class="social" style="color: var(--theme-title);"><i
-							class="fab fa-google-plus-g"></i>{{ __('Google Sign in') }}</a>
+							class="fab fa-google-plus-g"></i>{{ __('') }}</a>
 					<a href="#" class="social" style="color: var(--theme-title);"><i class="fab fa-linkedin-in"></i></a>
 				</div>
 				<span>or use your account</span>
