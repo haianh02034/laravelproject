d0ff6abb272d07831517bd629705f52561158cd3
nguyenvantung12d <tungnvbh00299@fpt.edu.vn>
2023-08-03 15:29:46 +0700

    edit html ticketbooking lan 2

--- resources/views/seat-selection.blade.php
+++ resources/views/seat-selection.blade.php
@@ -88,7 +88,7 @@
 									</a>
 									<ul class="dropdown-menu">
 										<li><a href="{{ asset('profile') }}">Account information</a></li>
-										<li><a href="{{ asset('logout') }}">Logcheckout</a></li>
+										<li><a href="{{ asset('logout') }}">Logout</a></li>
 									</ul>
 								</li>
 							@else
@@ -124,11 +124,7 @@
 										<li><a href="movies.html">Action</a></li>
 										<li><a href="movies.html">Drama</a></li>
 										<li><a href="movies.html">Family</a></li>
-										<li><a href="movies.html">Thriller</a></li>
-										<li><a href="movies.html">Commedy</a></li>
-										<li><a href="movies.html">Romantic</a></li>
-										<li><a href="movies.html">Tv-Series</a></li>
-										<li><a href="movies.html">Horror</a></li>
+										
                                         
 									</ul>
 								</div>
