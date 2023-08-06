d6255c4e03b98902127ddb6363743ea3bbbcb7f2
nguyenvantung12d <tungnvbh00299@fpt.edu.vn>
2023-08-06 13:48:59 +0700

    editticketbooking  lan 5

--- resources/views/ticket-booking.blade.php
+++ resources/views/ticket-booking.blade.php
@@ -107,7 +107,9 @@
 						<div class="theme-switch-wrapper">
 							<label class="theme-switch" for="checkbox">
 								<input type="checkbox" id="checkbox">
-								
+								<div class="mode-container">
+									<i class="gg-sun"></i>
+									<i class="gg-moon"></i>
 								</div>
 							</label>
 						</div>
